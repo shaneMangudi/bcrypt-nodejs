959f10222851562e233749cf9ba4070430844966
Noah Isaacson <noah@nisaacson.com>
2013-03-16 11:59:49 -0400

    added mocha-based sync tests

---
+++ test/sync-test.js
@@ -0,0 +1,111 @@
+var inspect = require('eyespect').inspector()
+var should = require('should')
+var assert = require('assert')
+var bCrypt = require("../bCrypt")
+describe('Test Sync', function () {
+  this.timeout('16s')
+  this.slow('8s')
+
+  var salt1, salt2
+  var secret = "super secret"
+  var hash1, hash2, hash3, hash4, hash5, hash6, hash7, hash8, hash9, hash0
+
+  var pw1, pw2, pw3, pw4, hash_pw1, hash_pw2, hash_pw3, hash_pw4
+  before(function () {
+    salt1 = bCrypt.genSaltSync(8)
+    should.exist(salt1, 'genSaltSync failed')
+    salt2 = bCrypt.genSaltSync(10)
+    should.exist(salt2, 'genSaltSync failed')
+
+    hash1 = bCrypt.hashSync('super secret', salt1, null)
+    inspect(hash1, 'hash1')
+    hash2 = bCrypt.hashSync('super secret', salt1, null)
+    inspect(hash2, 'hash2')
+    hash3 = bCrypt.hashSync('supersecret', salt1, null)
+    inspect(hash3, 'hash3')
+    hash4 = bCrypt.hashSync('supersecret', salt1, null)
+    inspect(hash4, 'hash4')
+    hash5 = bCrypt.hashSync('super secret', salt2, null)
+    inspect(hash5, 'hash5')
+    hash6 = bCrypt.hashSync('super secret', salt2, null)
+    inspect(hash6, 'hash6')
+    hash7 = bCrypt.hashSync('supersecret', salt2, null)
+    inspect(hash7, 'hash7')
+    hash8 = bCrypt.hashSync('supersecret', salt2, null)
+    inspect(hash8, 'hash8')
+    hash9 = bCrypt.hashSync('super secret', null, null)
+    inspect(hash9, 'hash9')
+    hash0 = bCrypt.hashSync('super secret', null, null)
+
+    pw1 = '\u6e2f'  // http://www.fileformat.info/info/unicode/char/6e2f/index.htm
+    pw2 = '港' // Character 0x6e2f same as pw1.
+    pw3 = '\u6f2f'  // http://www.fileformat.info/info/unicode/char/6f2f/index.htm
+    pw4 = '漯' // Character 0x6f2f same as pw3.
+
+    var salt = '$2a$05$0000000000000000000000'
+    hash_pw1 = bCrypt.hashSync(pw1, salt, null)
+    inspect(hash_pw1,' hash_pw1')
+    hash_pw2 = bCrypt.hashSync(pw2, salt, null)
+    inspect(hash_pw2,' hash_pw2')
+    hash_pw3 = bCrypt.hashSync(pw3, salt, null)
+    inspect(hash_pw3,' hash_pw3')
+    hash_pw4 = bCrypt.hashSync(pw4, salt, null)
+    inspect(hash_pw4,' hash_pw4')
+
+  })
+
+  it('should match first set of compares', function () {
+
+    assert.ok(bCrypt.compareSync('super secret', hash1), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('super secret', hash2), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('super secret', hash3), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('super secret', hash4), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('super secret', hash5), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('super secret', hash6), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('super secret', hash7), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('super secret', hash8), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('super secret', hash9), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('super secret', hash0), 'compareSync should return true')
+  })
+
+  it('should match second set of compares', function () {
+    assert.ok(!bCrypt.compareSync('supersecret', hash1), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('supersecret', hash2), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('supersecret', hash3), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('supersecret', hash4), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('supersecret', hash5), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('supersecret', hash6), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('supersecret', hash7), 'compareSync should return true')
+    assert.ok(bCrypt.compareSync('supersecret', hash8), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('supersecret', hash9), 'compareSync should return true')
+    assert.ok(!bCrypt.compareSync('supersecret', hash0), 'compareSync should return true')
+  })
+
+  it('hash_pw2 should compare correctly to pw1', function () {
+    assert.ok(bCrypt.compareSync(pw1, hash_pw1), 'compareSync should return true')
+  })
+  it('hash_pw2 should compare correctly to pw2', function () {
+    assert.ok(bCrypt.compareSync(pw2, hash_pw2), 'compareSync should return true')
+  })
+  it('hash_pw3 should compare correctly to pw3', function () {
+    assert.ok(bCrypt.compareSync(pw3, hash_pw3), 'compareSync should return true')
+  })
+  it('hash_pw4 should compare correctly to pw4', function () {
+    assert.ok(bCrypt.compareSync(pw4, hash_pw4), 'compareSync should return true')
+  })
+
+
+  it('hash_pw1 should be different than hash_pw3', function () {
+    hash_pw1.should.not.eql(hash_pw3, 'hash_pw1 should be different from hash_pw3')
+  })
+
+  it('hash_pw2 should be different than hash_pw4', function () {
+    hash_pw2.should.not.eql(hash_pw4, 'hash_pw2 should be different from hash_pw4')
+  })
+  it('hash_pw1 should be equal hash_pw2', function () {
+    hash_pw1.should.eql(hash_pw2, 'hash_pw1 should equal hash_pw2')
+  })
+  it('hash_pw3 should be equal hash_pw4', function () {
+    hash_pw3.should.eql(hash_pw3, 'hash_pw3 should equal hash_pw4')
+  })
+})
