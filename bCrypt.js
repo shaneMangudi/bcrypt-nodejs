5f3b49be2bbd028317c8ed6a82684167ea8b0220
Ben <benlowry@outlook.com>
2017-03-26 11:48:02 -0600

    Stripped redundant 'ret' var and try/catch

--- bCrypt.js
+++ bCrypt.js
@@ -233,17 +233,7 @@ var index_64 = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 		49, 50, 51, 52, 53, -1, -1, -1, -1, -1];
 
 function getByte(c) {
-	var ret = 0;
-	var b;
-	if(!c.charCodeAt) {
-		b = c;
-	} else {
-	    try {
-		b = c.charCodeAt(0);
-    	    } catch (err) {
-		b = c;
-	    }
-	}
+	var b = c && c.length && c.charCodeAt ? c.charCodeAt(0) : b;
 	if (b > 127) {
 		return -128 + (b % 128);
 	} else {
